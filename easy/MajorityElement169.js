635fbbbc300de34c5b274e1cf5a5636c39b7938e
Liu <eva.liu@sap.com>
2021-04-12 11:25:07 +0800

    add description

--- easy/MajorityElement169.js
+++ easy/MajorityElement169.js
@@ -1,3 +1,14 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
 /**
  * @param {number[]} nums
  * @return {number}
